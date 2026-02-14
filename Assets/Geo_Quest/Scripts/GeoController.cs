02cdd94db3a74eb3fb955e9321bebd8801b2ae31
Lucasox3 <lucasaoyero@icloud.com>
2026-02-14 16:28:51 -0500

    Day 3

---
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeoController : MonoBehaviour
+{
+
+    string rubix = "Hello guys";
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello Wold");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
